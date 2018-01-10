8e125cedcd2bc254441c4b51a35f9a213b010ac8
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2018-01-10 10:48:22 +0100

    Implemented GuestList class

--- lib/guest_list.rb
+++ lib/guest_list.rb
@@ -1,7 +1,40 @@
 require 'json'
 
 class GuestList
+  DISTANCE_LIMIT = 100.0
+
   def collect(file)
-    
+    @distance_obj = DistanceToPoint.new
+    load_customers(file)
+    sort_asc(select_guests)
+  end
+
+  private
+
+  def sort_asc(guests)
+    guests.sort
+  end
+
+  def load_customers(file)
+    @customers = []
+    if file.nil? || !File.exists?(file)
+      raise ArgumentError.new("Please provide customers file")
+    else
+      File.open(file).each do |line|
+        @customers << JSON.parse(line)
+      end
+    end
+
+  end
+
+  def select_guests
+    result = {}
+    @customers.each do |customer_data|
+      if @distance_obj.calculate(customer_data['latitude'].to_f, customer_data['longitude'].to_f) <= DISTANCE_LIMIT
+        result[customer_data['user_id']] = customer_data['name']
+      end
+    end
+
+    result
   end
 end
---
+++ spec/guest_list_spec.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/guest_list'
+
+describe GuestList do
+  it 'should return argument error if file was not provided'
+  it 'should return argument error if file was not exist'
+end
