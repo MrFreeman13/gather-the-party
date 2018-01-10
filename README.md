784ba1086ab5bbdf78760d8e51ac82ef83000781
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2018-01-10 14:09:55 +0100

    Updated README, refactored GatherTheParty#run

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Script for gather parties
 
-..
+Use this script to get sorted list of guests which are not far from you
+(100 km by default) to invite them for a party
 
 # Running
 
--- lib/gather_the_party.rb
+++ lib/gather_the_party.rb
@@ -1,6 +1,6 @@
 class GatherTheParty
   def run(file)
     guest_list = GuestList.new.collect(file)
-    puts "Here's your members list #{guest_list.inspect}"
+    puts "Here's your guest list: \n #{guest_list}"
   end
 end
